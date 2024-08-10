c00b020a287198727bef2dfbba156a4999ac54de
sairesifontes <sairesifontes@gmail.com>
2024-08-10 12:52:13 -0300

    entrega

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import cookieParser from "cookie-parser";
 import envs from "./config/env.config.js";
 import cors from "cors";
 import { errorHandle } from "./errors/errorHandle.js"
-
+import { logger } from "./utils/logger.js";
 
 connectMongoDB();
 
@@ -35,6 +35,25 @@ initializePassport();
 app.use(cors());
 
 app.use("/api", router);
+
+app.get("/operacionsencilla", (req, res) => {
+  let sum = 0;
+  for (let i = 0; i < 100000; i++) {
+    sum += i;
+  }
+
+  res.send({ sum });
+});
+
+app.get("/operacioncompleja", (req, res) => {
+  let sum = 0;
+  for (let i = 0; i < 5e8; i++) {
+    sum += i;
+  }
+
+  res.send({ sum });
+});
+
 app.use(errorHandle);
 
 app.listen(envs.PORT, () => {
--- src/errors/errorHandle.js
+++ src/errors/errorHandle.js
@@ -1,7 +1,11 @@
+import { logger } from "../utils/logger.js";
+
 export const errorHandle = (err, req, res, next) => {
   const status = err.status || 500;
   const message = status === 500 ? "Internal Server Error" : err.message;
-
+  if(status === 500) {
+    logger.log("error", err.message);
+  }
   res.status(status).json({
     error: {
       message,
